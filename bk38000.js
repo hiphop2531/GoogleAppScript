c14ba1fb9206e7d970fd42286134153e51fd11af
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2024-02-05 15:03:32 +0700

    Update bk38000.js

--- bk38000.js
+++ bk38000.js
@@ -1,3 +1,3 @@
 function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/buengkandol" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">สำนักงานที่ดินจังหวัดบึงกาฬ &copy; บึงกาฬ <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/buengkandol" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2024 <a style="color:gray;text-decoration:none" href="https://www.dol.go.th/buengkan/Pages/default.aspx" target="_blank"> Contact</a></center>'
   }
