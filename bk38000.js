10dd3cdbcabadefb5feec433fea1f6bb9cd31709
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2024-02-05 11:53:24 +0700

    Create bk38000.js

---
+++ bk38000.js
@@ -0,0 +1,3 @@
+function setFooter(){
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/buengkandol" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">สำนักงานที่ดินจังหวัดบึงกาฬ &copy; บึงกาฬ <a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/p/examvip.html" target="_blank"> Contact</a></center>'
+  }
