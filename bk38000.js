fc8d9d852194e2e8343a1974104134cc1209bf41
hiphop2531 <30566327+hiphop2531@users.noreply.github.com>
2024-02-05 15:15:39 +0700

    Delete bk38000.js

--- bk38000.js
+++
@@ -1,3 +0,0 @@
-function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/buengkandol" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Examblog &copy; 2024 <a style="color:gray;text-decoration:none" href="https://www.dol.go.th/buengkan/Pages/default.aspx" target="_blank"> Contact</a></center>'
-  }
